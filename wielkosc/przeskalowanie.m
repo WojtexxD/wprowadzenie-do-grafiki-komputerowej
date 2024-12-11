31c82643d9fa52c66d5fce7fe5d756e8a47bdcfc
Wojciech <115220226+WojtexxD@users.noreply.github.com>
2024-12-11 14:34:59 +0100

    Add files via upload

---
+++ wielkosc/przeskalowanie.m
@@ -0,0 +1,6 @@
+x=6.5;
+zaokr(x)
+
+#x_we(x_wy)=zaokr(1+((x_wy-1)*(s_we-1)/(s_wy-1)));
+#y_we(y_wy)=zaokr(1+((y_wy-1)*(w_we-1)/(w_wy-1)));
+
---
+++ wielkosc/zaokr.m
@@ -0,0 +1,3 @@
+function zaokr (v)
+  round(v)
+endfunction
